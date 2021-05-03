c0ba0841b7cf4b64f7017dd87afab406b4bdac32
Shane Mulligan <mullikine@gmail.com>
2021-05-03 19:27:51 +1200

    Mon 03 May 2021 19:27:51

--- src/open_in_clojure/core.clj
+++ src/open_in_clojure/core.clj
@@ -1,6 +1,12 @@
 (ns open-in-clojure.core
   (:gen-class))
 
+(defn fp fptype
+  "Opens the file given by the path into a clojure object."
+  []
+  )
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
